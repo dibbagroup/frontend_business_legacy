1d17057d7752399ef45de7f22439aaef57318362
Igor Rosiak <86728719+Igorrosiak@users.noreply.github.com>
2022-09-07 22:28:14 -0300

    Updating

--- src/views/home/homeView.jsx
+++ src/views/home/homeView.jsx
@@ -1,4 +1,12 @@
+import axios from "axios"
+import { useEffect } from "react"
+
 export const HomeView = _ => {
+
+    useEffect (()=>{
+        axios.get("http://localhost:9090/events").then((res)=>{console.log(res.data)})
+    },[])
+
     return (
         <main>
             <h1>Home</h1>

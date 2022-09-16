da3be9915d6a6ff864ed370cd472e10ac62162f6
Carlos Amaral <carlosamaral@Carloss-MacBook-Pro.local>
2022-09-16 16:02:58 -0300

    Adding first part of the formg

--- src/views/new-event/newEventView.jsx
+++ src/views/new-event/newEventView.jsx
@@ -1,335 +1,285 @@
-import { useState, useEffect } from "react"
-import { Button } from "react-bootstrap"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  FloatingLabel,
+  Form,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
+import axios from "axios";
 
-import "./newEventView.scss"
-
-/*
-MARK: ETAPA 1-3
-str  : Nome,
-str  : Genero,
-str  : Descricao,
-img  : Banner,
-date : Data,
-hour : Horario inicio,
-hour : Horario fim,
-str  : CEP,
-str  : UF,
-str  : Cidade,
-str  : Logradouro,
-int  : Número,
-str  : Complemento,
-
-MARK: ETAPA 2-3
-int  : Classificacao de idade indicativa
-str  : Gênero de ingressos
-int  : Qtd de lotes
-str  : Categoria dos ingressos
-img  : Foto das categorias da festa
-date : Inicio do lote
-date : Fim do lote
-
-MARK: ETAPA 3-3
-bool : Taxas
-int  :  D+30 - D+15
-*/
+import "./newEventView.scss";
+import { Header } from "../../components/header/header";
 
 class Place {
-    cep = String
-    uf = String
-    city = String
-    street = String
-    number = 0
-    complement = String
+  cep = String;
+  uf = String;
+  city = String;
+  street = String;
+  number = 0;
+  complement = String;
 }
 
 class Lot {
-    startDate = Date
-    endDate = Date
-    ticketGender = String /* (Unissex) ou (Fem e Masc) */
-    loteType = String /* (Individual) ou (Area) */
+  startDate = Date;
+  endDate = Date;
+  ticketGender = String; /* (Unissex) ou (Fem e Masc) */
+  loteType = String; /* (Individual) ou (Area) */
 }
 
 class Event {
-    name = String
-    gender = String
-    description = String
-    bannerImage = Image
-    startDate = Date
-    startTime = Date
-    endTime = Date
-    place = new Place()
+  name = String;
+  gender = String;
+  description = String;
+  bannerImage = Image;
+  startDate = Date;
+  startTime = Date;
+  endTime = Date;
+  place = new Place();
 
-    minimumAge = 0
-    imageCategorys = Image
-    qtdLots = 0
-    lots = [new Lot()]
+  minimumAge = 0;
+  imageCategorys = Image;
+  qtdLots = 0;
+  lots = [new Lot()];
 
-    taxes = Boolean
-    payment = 0 /* D+15 ou D+30 */
+  taxes = Boolean;
+  paymentMethod = 0; /* D+15 ou D+30 */
 }
 
-let eventObject = new Event()
+let eventObject = new Event();
 
-const getCEP = () => {
+export const NewEventView = (_) => {
+  const [step, setStep] = useState(0);
+  const [street, setStreet] = useState("Rua");
+  const [complement, setComplement] = useState("Complemento");
+  const [neighborhood, setNeighborhood] = useState("Bairro");
+  const [city, setCity] = useState("Cidade");
+  const [uf, setUF] = useState("UF");
+  const [ddd, setDDD] = useState("ddd");
+  const [number, setNumber] = useState("Number");
+
+  const getCEP = (cep) => {
     // https://h-apigateway.conectagov.estaleiro.serpro.gov.br/api-cep/v1/consulta/cep/60130240
-    const cep = document.getElementById(`cep`).value
     if (cep.length === 8) {
-        axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-        .then((res) => {
-            console.log(res.data)
-        })
+      cep = cep.replace("-", "");
+      axios.get(`https://viacep.com.br/ws/${cep}/json/`).then((res) => {
+        setStreet(res.data.logradouro);
+        setNeighborhood(res.data.bairro);
+        setCity(res.data.localidade);
+        setUF(res.data.uf);
+        setDDD(res.data.ddd);
+      });
+    } else {
+      setStreet("Rua");
+      setNeighborhood("Bairro");
+      setCity("Cidade");
+      setUF("UF");
+      setDDD("ddd");
     }
-}
+  };
 
-let currentStep = 0
-export const NewEventView = _ => {
-    const [step, setStep] = useState(0)
+  return (
+    <main className="p-5">
+      {/* HEADER */}
 
-    return (
-        <main>
-            {/* HEADER */}
-            
-            {step === 0 && <StepOne />}
-            {step === 1 && <StepTwo />}
-            {step === 2 && <StepThree />}
+      <div>
+        <Row>
+          <Col>
+            <h2 className="text-dark">Etapa {step + 1}/3</h2>
+          </Col>
 
-            {/* FOOTER */}
-        </main>
-    )
-}
+          {step >= 1 && (
+            <Col xs={1}>
+              <Button
+                variant="outline-dark"
+                onClick={() => {
+                  setStep(step - 1);
+                }}
+              >
+                Voltar
+              </Button>
+            </Col>
+          )}
 
-const StepOne = _ => {
-    return (
-        <main className="form-1">
-            <h1>Step 1</h1>
-            <Button onClick={() => { currentStep++ }}>Next</Button>
-        
-            <h1 className="text-title">Cadastre seu evento</h1>
+          <Col xs={1}>
+            <Button
+              variant="outline-success"
+              onClick={() => {
+                setStep(step + 1);
+              }}
+            >
+              Avançar
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p>
+              Para começar, nos conte um pouco sobre as informações do seu
+              evento. lembre-se que essa parte é como ficará visível para o
+              público da nossa plataforma
+            </p>
+          </Col>
+          <Col />
+        </Row>
+      </div>
 
-            <form className="form-1">
-      
-                <h2 className="text-etapa">Etapa 1/4</h2>
+      <hr className="my-4" />
 
-                <p className="text-subtitle">
-                Para começar, nos conte um pouco sobre as informações do seu evento. 
-                lembre-se que essa parte é como ficará visível para o público da nossa plataforma
-                </p>
+      {step === 0 && (
+        <form>
+          <Row>
+            {/* LEFT SIDE */}
+            <Col>
+              <h4>Detalhes</h4>
 
-                <div class="flex">
-                    <div className="form-group pt-3 inputFlex">
-                        <small>Nome do evento</small>
-                        <input
-                            /* onChange={(ev) => handle(ev)}
-                            value={formEvent.eventName || ""} */
-                            type="text"
-                            className="form-control"
-                            id="name"
-                            name="eventName"
-                            placeholder="No máximo 100 caracteres"
-                            maxLength={100}
-                            required={true}
-                        />
-                        <br />
-                    </div>
+              <Row>
+                <Col>
+                  <FloatingLabel
+                    controlId="floatingInput"
+                    label="Título"
+                    className="mb-3"
+                  >
+                    <Form.Control type="text" placeholder="Título" />
+                  </FloatingLabel>
+                </Col>
 
-                    <div className="form-group pt-3 inputFlex">
-                        <small>Gênero Musical</small> 
-                        <input type="text"
-                        className="form-control"
-                        placeholder="Funk, Rap, Pagode..."></input>
-                    </div>
-                </div>
+                <Col>
+                  <FloatingLabel
+                    controlId="floatingSelect"
+                    label="Gênero musical"
+                    className="mb-3"
+                  >
+                    <Form.Select aria-label="Gênero musical">
+                      <option>Escolha...</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>
+                  </FloatingLabel>
+                </Col>
+              </Row>
 
-                <div class="flex">
-                    <div className="form-group mb-3 inputFlex">
-                        <label>Descrição</label>
-                        <input
-                            /* onChange={(ev) => handle(ev)}
-                            value={formEvent.description || ""} */
-                            className="form-control"
-                            id="description"
-                            name="description"
-                            placeholder="Conte sobre seu evento"
-                            rows={5}
-                            required={true}
-                        />
-                    </div>
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Descrição"
+                className="mb-3"
+              >
+                <Form.Control
+                  type="text"
+                  placeholder="Descrição"
+                  as="textarea"
+                  style={{ height: "130px" }}
+                />
+              </FloatingLabel>
 
-                    <div className="custom-file form-group inputFlex">
-                        <input
-                            type="file"
-                            /* className="custom-file-input"
-                            multiple={false}
-                            onDone={({ base64 }) =>
-                            setFormEvent({ ...formEvent, selectedFile: base64 })
-                            } */
-                        />
-                        <label className="custom-file-label">
-                            Anexe aqui uma foto de banner da sua festa...
-                        </label>
-                    </div>
-                </div>
+              <Form.Control type="file" size="md" className="mb-3" />
+            </Col>
 
-                <div className="form-group">
-            
-                    <h3 className="text-center pt-5">Datas e Horários</h3>
+            {/* RIGHT SIDE */}
+            <Col>
+              <h4>Local</h4>
+              <FloatingLabel label="CEP" className="mb-3">
+                <Form.Control
+                  type="text"
+                  placeholder="CEP"
+                  id="new-event-cep"
+                  maxLength={9}
+                  onChange={() => {
+                    getCEP(document.getElementById("new-event-cep").value);
+                  }}
+                />
+              </FloatingLabel>
 
-                    <small className="control-label">Data</small>
-                    <input
-                    /* onChange={(ev) => handle(ev)}
-                    value={formEvent.date || ""} */
-                    className="form-control"
-                    id="date"
-                    name="date"
-                    type="date"
-                    required={true}
+              <Row>
+                <Col xs={3}>
+                  <FloatingLabel label={uf} className="mb-3">
+                    <Form.Control type="text" placeholder="Rua" disabled />
+                  </FloatingLabel>
+                </Col>
+                <Col>
+                  <FloatingLabel label={city} className="mb-3">
+                    <Form.Control type="text" placeholder="Rua" disabled />
+                  </FloatingLabel>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={3}>
+                  <FloatingLabel label={neighborhood} className="mb-3">
+                    <Form.Control type="text" placeholder="Bairro" disabled />
+                  </FloatingLabel>
+                </Col>
+
+                <Col>
+                  <FloatingLabel label={street} className="mb-3">
+                    <Form.Control type="text" placeholder="Rua" disabled />
+                  </FloatingLabel>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={3}>
+                  <FloatingLabel label="Número" className="mb-3">
+                    <Form.Control
+                      type="number"
+                      id="new-event-number"
+                      placeholder="Número"
+                      required
+                      onChange={() => {
+                        setNumber(
+                          document.getElementById("new-event-number").value
+                        );
+                      }}
                     />
-                </div>
+                  </FloatingLabel>
+                </Col>
 
-                <div className="form-row">
-                    <div className="form-group col-md-6">
-                        <small>Horário de início</small>
-                        <input
-                        /* onChange={(ev) => handle(ev)}
-                        value={formEvent.startTime || ""} */
-                        type="time"
-                        id="start"
-                        name="startTime"
-                        className="form-control"
-                        placeholder="Horário"
-                        required={true}
-                        />
-                    </div>
+                <Col>
+                  <FloatingLabel label="Complemento" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      id="new-event-complement"
+                      placeholder="Complemento"
+                      onChange={() => {
+                        setComplement(
+                          document.getElementById("new-event-complement").value
+                        );
+                      }}
+                    />
+                  </FloatingLabel>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </form>
+      )}
+      {step === 1 && <StepTwo />}
+      {step === 2 && <StepThree />}
 
-                    <div className="form-group col-md-6">
-                        <small>Horário de encerramento</small>
-                        <input
-                        /* onChange={(ev) => handle(ev)}
-                        value={formEvent.endTime || ""} */
-                        type="time"
-                        id="finish"
-                        name="endTime"
-                        className="form-control"
-                        placeholder="Horário"
-                        required={true}
-                        />
-                    </div>
-                </div>
+      {/* FOOTER */}
+    </main>
+  );
+};
 
-                <div className="form-group">
-                    <h3 className="text-center pt-5">Local</h3>
-                </div>
+const StepOne = (_) => {
+  return <main>{/* <Header/> */}</main>;
+};
 
-                <div class="flex">
-                    <div className="form-group col-md-6 inputFlex">
-                        <small>CEP</small>
-                        <input
-                            /* onChange={(ev) => handle(ev)}
-                            value={formEvent.CEP || ""} */
-                            type="text"
-                            id="cep"
-                            name="CEP"
-                            className="form-control"
-                            required={true}
-                        />
-                    </div>
+const StepTwo = (_) => {
+  return (
+    <main>
+      <h1>Step 2</h1>
+    </main>
+  );
+};
 
-                    <div className="form-row inputFlex">
-                        <small>UF</small>
-                        
-                        <select
-                            /* onChange={(ev) => handle(ev)}
-                            value={formEvent.state || ""} */
-                            id="state"
-                            name="state"
-                            className="form-control"
-                        >
-                            <option defaultValue>SC</option>
-                            <option>SP</option>
-                            <option>PR</option>
-                            <option>RS</option>
-                            <option>RJ</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="flex">
-                    <div className="form-group col-md-6 inputFlex">
-                        <small>Cidade</small>
-                        <select
-                            /* onChange={(ev) => handle(ev)}
-                            value={formEvent.city || ""} */
-                            name="city"
-                            id="city"
-                            className="form-control"
-                        >
-                            <option defaultValue>Escolher...</option>
-                            <option>...</option>
-                            <option>Florianópolis</option>
-                            <option>São Paulo</option>
-                        </select>
-                    </div>
-
-                    <div className="form-group col-md-6">
-                        <small>Logradouro</small>
-                        <input
-                            /* onChange={(ev) => handle(ev)}
-                            value={formEvent.address || ""} */
-                            type="text"
-                            id="address"
-                            name="address"
-                            className="form-control"
-                            placeholder="Rua ..."
-                            required={true}
-                        />
-                    </div>
-                </div>
-
-                <div class="flex">
-                    <div className="form-group col-md-6 inputFlex">
-                        <small>Número</small>
-                        <input
-                            /* onChange={(ev) => handle(ev)}
-                            value={formEvent.complement || ""} */
-                            type="text"
-                            id="complement"
-                            name="complement"
-                            className="form-control"
-                            placeholder="03"
-                            required={true}
-                        />
-                    </div>
-
-                    <div className="form-group col-md-6 inputFlex">
-                        <small>Complemento</small>
-                        <input 
-                        placeholder="Casa"
-                        className="form-control"></input>
-                    </div>
-                </div>
-
-                <button className="next">Passar</button>
-
-            </form>
-        </main>
-    )
-}
-
-const StepTwo = _ => {
-    return (
-        <main>
-            <h1>Step 2</h1>
-            <Button onClick={() => { currentStep++ }}>Next</Button>
-        </main>
-    )
-}
-
-const StepThree = _ => {
-    return (
-        <main>
-            <h1>Step 3</h1>
-            <Button onClick={() => { currentStep++ }}>Next</Button>
-        </main>
-    )
-}
+const StepThree = (_) => {
+  return (
+    <main>
+      <h1>Step 3</h1>
+    </main>
+  );
+};

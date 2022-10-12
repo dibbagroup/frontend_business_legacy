f916417a9da916e20f85b16ff9d112a61c85e420
Carlos Amaral <carlosamaral@Carloss-MacBook-Pro.local>
2022-10-12 15:56:36 -0300

    updating/Adding routes on the project

---
+++ src/views/dashboard/dashboardView.jsx
@@ -0,0 +1,9 @@
+import "./dashboardView.scss";
+
+export const DashboardView = _ => {
+    return (
+        <main>
+            <h1>Dashboard</h1>
+        </main>
+    )
+}
---
+++ src/views/onboarding/onboardingView.jsx
@@ -0,0 +1,9 @@
+import "./onboardingView.scss";
+
+export const OnboardingView = _ => {
+    return (
+        <main>
+            <h1>Onboarding</h1>
+        </main>
+    )
+}
